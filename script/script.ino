d94a078290f7438ddabcf143ca503f8c89a4a42b
Tyler Chen <tyche4490@gmail.com>
2023-06-05 12:30:54 -0700

    changed hardware config and libraries

--- script/script.ino
+++ script/script.ino
@@ -1,31 +1,50 @@
 
-#include <SPI.h>
-#include <SD.h>
-#include <TMRpcm.h>
+//#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <EEPROM.h>
+#include <RtcDS1302.h>
+#define SPEAKER 10
+#define SCREEN_ADDRESS 0x3C
+#define OLED_RESET -1
+#define RTCCLK 7
+#define RTCDAT 8
+#define RTCRST 9
+//static const unsigned char PROGMEM image_data_Saraarray[1024] = {
 
-#define SPEAKER 3
-#define SDCS 9
+
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
+
+ThreeWire myWire(RTCDAT, RTCCLK, RTCRST); 
+RtcDS1302<ThreeWire> Rtc(myWire);
+
 
 byte holdBuffer[3] = {0, 0, 0};
 
-
 void setup() {
   Serial.begin(115200);
+
+  Rtc.Begin();
+
   // buttons
   for (int pin=4; pin<=6; pin++) {
     pinMode(pin, INPUT);
   }
 
-  if (!SD.begin(SDCS)) {
-    Serial.print("Fuck");
+  pinMode(SPEAKER, OUTPUT);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 failed to start"));
     while (1);
   }
-  Serial.print("SD card workie workie");
+  display.display();
+
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   for (int buttonPin=4; buttonPin<=6; buttonPin++) {
     if (digitalRead(buttonPin) == HIGH) {
       holdBuffer[buttonPin-4] = min(3, holdBuffer[buttonPin-4] + 1);    
@@ -40,5 +59,6 @@ void loop() {
     
   }
   delay(50);
+  
 
 }
